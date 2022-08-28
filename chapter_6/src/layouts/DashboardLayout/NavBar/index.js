f6c6230f93031c061e9c049d1eecf61acecfa559
Neo-Hao <qianghaoqiang@gmail.com>
2022-08-27 19:34:17 -0700

    chapter 6 code ready

---
+++ chapter_6/src/layouts/DashboardLayout/NavBar/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-use-before-define */
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Drawer } from "@mui/material";
+import PropTypes from "prop-types";
+import Content from "./Content";
+
+const NavBar = ({ onMobileClose, openMobile }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (openMobile) onMobileClose();
+  }, [location.pathname]);
+
+  // mobile navigation first
+  // desktop navigation second
+  return (
+    <>
+      <Drawer
+        anchor="left"
+        sx={{ width: 256, display: { xs: "block", md: "none" } }}
+        open={openMobile}
+        variant="temporary"
+      >
+        <Content />
+      </Drawer>
+
+      <Drawer
+        anchor="left"
+        open
+        variant="persistent"
+        sx={{
+          width: 256,
+          display: { xs: "none", md: "block" },
+        }}
+      >
+        <Content />
+      </Drawer>
+    </>
+  );
+};
+
+NavBar.propTypes = {
+  onMobileClose: PropTypes.func,
+  openMobile: PropTypes.bool,
+};
+
+export default NavBar;
